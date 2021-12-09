6ec75fc7f420f4e7812fccad61d1a016ed8a6639
Denzel <dbartolaba123@gmail.com>
2021-12-09 10:45:45 -0800

    mushroom thing

--- data-utils.js
+++ data-utils.js
@@ -1,5 +1,11 @@
 export default function findFriendByName(name, friends) {
     // iterate through the friends
-    
-    // when you find a friend with the correct name, return that friend
+    for (let friend of friends) {
+        if (friend.name === name) {
+            return friend;
+        }
+    }
 }
+
+   
+    // when you find a friend with the correct name, return that friend
